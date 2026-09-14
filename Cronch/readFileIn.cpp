be3015d24d9c121ea584ab86d1c47e15dba28ec5
Cavan Theiss <cavanrt@gmail.com>
2026-09-14 16:21:02 -0500

    Update 14 readFileIn.cpp
    14

--- Cronch/readFileIn.cpp
+++ Cronch/readFileIn.cpp
@@ -16,5 +16,6 @@ std::vector<uint8_t> read_binary_file(const std::string& filename){
   file.seekg(0, std::ios::beg);
 
   // Resize vector to fit file content
+  std::vector<uint8_t> buffer(static_cast<size_t>(size));
   
 }
