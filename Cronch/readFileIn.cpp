69b78e2b421fce2049b60c9068084e202d1b6adb
Cavan Theiss <cavanrt@gmail.com>
2026-09-14 16:16:17 -0500

    Update 7 readFileIn.cpp
    7

--- Cronch/readFileIn.cpp
+++ Cronch/readFileIn.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cstdint>
 
-std::vector<uint8_t> read_binary_file(){
+std::vector<uint8_t> read_binary_file(const std::string& filename){
 
   // Open file in binary mode
   std::ifstream file(filename, std::ios::binary | std::ios::ate);
