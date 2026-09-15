062eed8e4b27d04ebff5a2a5e70b547ddcdbc97d
Cavan Theiss <cavanrt@gmail.com>
2026-09-15 16:47:52 -0500

    Update 18 buildHuffmanTree.cpp
    18

--- Cronch/buildHuffmanTree.cpp
+++ Cronch/buildHuffmanTree.cpp
@@ -19,6 +19,10 @@ HuffmanNode(uint32_t f, std::unique_ptr<HuffmanNode> l, std::unique_ptr<HuffmanN
 // Min-heap: compare by frequency (smaller = higher priority)
 struct Compare {
 
+  bool operator()(const std::unique_ptr<HuffmanNode>& a, const std::unique_ptr<HuffmanNode>& b) const {
 
+      
+    
+  }
 
 };
