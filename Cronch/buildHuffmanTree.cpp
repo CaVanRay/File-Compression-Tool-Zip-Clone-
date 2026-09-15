dbd85c582fae589d6f8490aaff7e040040926d7f
Cavan Theiss <cavanrt@gmail.com>
2026-09-15 16:43:20 -0500

    Update 15 buildHuffmanTree.cpp
    15

--- Cronch/buildHuffmanTree.cpp
+++ Cronch/buildHuffmanTree.cpp
@@ -12,6 +12,6 @@ std::unique_ptr<HuffmanNode> left;
 std::unique_ptr<HuffmanNode> right;
 
 HuffmanNode(uint32_t f, uint8_t s) : freq(f), symbol(s), is_leaf(true) {}
-HuffmanNode(uint32_t f, std::unique_ptr<HuffmanNode> l, std::unique_ptr<HuffmanNode> r)
+HuffmanNode(uint32_t f, std::unique_ptr<HuffmanNode> l, std::unique_ptr<HuffmanNode> r) : freq(f), symbol(0), is_leaf(false), left(std::move(l)), right(std::move(r)) {}
 
 };
