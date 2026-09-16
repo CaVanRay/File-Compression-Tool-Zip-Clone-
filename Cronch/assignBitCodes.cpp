ab28777e6b35e39cfce37ac9fab583365f1c1588
Cavan Theiss <cavanrt@gmail.com>
2026-09-16 08:37:19 -0500

    Update 24 assignBitCodes.cpp
    24

--- Cronch/assignBitCodes.cpp
+++ Cronch/assignBitCodes.cpp
@@ -5,5 +5,6 @@
 
 // Code table: index = byte value, value = bit string
 struct CodeTable {
-
+    std::string code[256]; // e.g. code[72] = "010"
+    uint8_t     len[256]; // code length in bits (0 if unused)
 };
