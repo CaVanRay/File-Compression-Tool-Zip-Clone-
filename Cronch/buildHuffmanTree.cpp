76e47bc66e4852d7cf31cc1f5fc587b7a123f27d
Cavan Theiss <cavanrt@gmail.com>
2026-09-16 08:30:07 -0500

    Update 17 buildHuffmanTree.cpp
    17

--- Cronch/buildHuffmanTree.cpp
+++ Cronch/buildHuffmanTree.cpp
@@ -51,5 +51,7 @@ std::unique_ptr<HuffmanNode> build_huffman_tree(const uint32_t freq[256]) {
       auto hi = std::move(pq.top()); pq.pop();
       pq.push(std::make_unique<HuffmanNode>(lo->freq + hi->freq, std::move(lo), std::move(hi)));
   }
+
+  return std::move(pq.top());
   
 }
