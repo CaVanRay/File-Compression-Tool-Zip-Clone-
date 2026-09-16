431ea78f0f9695f4a6552808aca2e6817cf7f407
Cavan Theiss <cavanrt@gmail.com>
2026-09-16 08:12:32 -0500

    Update 4 buildHuffmanTree.cpp
    4

--- Cronch/buildHuffmanTree.cpp
+++ Cronch/buildHuffmanTree.cpp
@@ -25,8 +25,9 @@ struct Compare {
 
 std::unique_ptr<HuffmanNode> build_huffman_tree(const uint32_t freq[256]) {
     using PQ = std::priority_queue<
-        std::unique_ptr<HuffmanNode>, std::vector<std::unique_ptr<HuffmanNode>>,
+        std::unique_ptr<HuffmanNode>,
+        std::vector<std::unique_ptr<HuffmanNode>>,
         Compare
-  >;
+    >;
   
 }
