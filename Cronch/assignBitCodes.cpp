5b431b5f9ac420d6fc2aa821fe62b554818c4b3b
Cavan Theiss <cavanrt@gmail.com>
2026-09-16 08:38:44 -0500

    Update next-to-last assignBitCodes.cpp

--- Cronch/assignBitCodes.cpp
+++ Cronch/assignBitCodes.cpp
@@ -8,3 +8,7 @@ struct CodeTable {
     std::string code[256]; // e.g. code[72] = "010"
     uint8_t     len[256]; // code length in bits (0 if unused)
 };
+
+Void assign_codes(const HuffmanNode* node, const std::string& prefix, CodeTable& table){
+    
+}
