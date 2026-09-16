6437501f298c9221fe723faffd65f9adfffe6615
Cavan Theiss <cavanrt@gmail.com>
2026-09-16 08:26:54 -0500

    Update 14 buildHuffmanTree.cpp
    14

--- Cronch/buildHuffmanTree.cpp
+++ Cronch/buildHuffmanTree.cpp
@@ -47,7 +47,7 @@ std::unique_ptr<HuffmanNode> build_huffman_tree(const uint32_t freq[256]) {
 
   // Merge until one node remains
   while (pq.size() > 1) {
-    
+      auto lo = std::move(pq.top()); pq.pop();
   }
   
 }
