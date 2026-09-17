6f89b181e8e046bcb46e9906a31687da2b242316
Cavan Theiss <cavanrt@gmail.com>
2026-09-17 15:38:04 -0500

    Update 1 assignBitCodes.cpp
    1

--- Cronch/assignBitCodes.cpp
+++ Cronch/assignBitCodes.cpp
@@ -9,7 +9,7 @@ struct CodeTable {
     uint8_t     len[256]; // code length in bits (0 if unused)
 };
 
-Void assign_codes(const HuffmanNode* node, const std::string& prefix, CodeTable& table){
+void assign_codes(const HuffmanNode* node, const std::string& prefix, CodeTable& table){
     if (!node) return;
 
     if (node->is_leaf) {
