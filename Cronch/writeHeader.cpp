e5350aee538d358d9805380ee7b95c0ac434b6fb
Cavan Theiss <cavanrt@gmail.com>
2026-09-18 15:27:05 -0500

    Update 4 writeHeader.cpp
    4

--- Cronch/writeHeader.cpp
+++ Cronch/writeHeader.cpp
@@ -1,4 +1,4 @@
 // Simple approach: write 256 bytes of code lengths, the bitstream
 void write_header(std::ofstream& file, const CodeTable& table) {
-  
+    // 256 bytes, one per symbol: the code length in bits
 }
