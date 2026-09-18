5c57bea7d0c1a72abeac10b1d8db70cd24feeec6
Cavan Theiss <cavanrt@gmail.com>
2026-09-18 15:28:40 -0500

    Update writeHeader.cpp

--- Cronch/writeHeader.cpp
+++ Cronch/writeHeader.cpp
@@ -3,5 +3,5 @@ void write_header(std::ofstream& file, const CodeTable& table) {
     // 256 bytes, one per symbol: the code length in bits
     // (0 means the symbol never appeared)
     for (int i = 0; i < 256; i++)
-        
+        file.put(static_cast<char>(table.len[i]));
 }
