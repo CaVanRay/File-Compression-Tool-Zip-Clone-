548d989f5c943e6b2ebad06b311ebcb3d66dda27
Cavan Theiss <cavanrt@gmail.com>
2026-09-20 17:24:46 -0500

    Update 18 countBytes.cpp
    18

--- Cronch/countBytes.cpp
+++ Cronch/countBytes.cpp
@@ -1,6 +1,4 @@
-#include <vector>
-#include <cstdint>
-#include <cstring>
+#include "cronchHeaders.h"
 
 void count_frequencies(const std::vector<uint8_t>& data, uint32_t freq[256]) {
     std::memset(freq, 0, sizeof(uint32_t) * 256);
