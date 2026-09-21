83bf8ac366aab654fe8b2c89f819681375a1f9cf
Cavan Theiss <cavanrt@gmail.com>
2026-09-21 08:04:02 -0500

    Update 12 UsePuts.cpp
    added the "cronch"

--- Cronch/source/UsePuts.cpp
+++ Cronch/source/UsePuts.cpp
@@ -2,10 +2,18 @@
 
 
 void optionsMenu(){
-
+  
   bool cont = true;
 
   do {
+    clearScreen();
+    std::cout << std::endl;
+std::cout << "|******   CCCC    R        OOOO     N    N    CCCC   H    H  ******|" << std::endl;
+std::cout << "|******  C    C   RRRR    O    O    NN   N   C    C  H    H  ******|" << std::endl;
+std::cout << "|****** C         R   R  O      O   N N  N  C        HHHHHH  ******|" << std::endl;
+std::cout << "|****** C         R      O      O   N  N N  C        H    H  ******|" << std::endl;
+std::cout << "|******  C    C   R       O    O    N   NN   C    C  H    H  ******|" << std::endl;
+std::cout << "|******   CCCC    R        OOOO     N    N    CCCC   H    H  ******|" << std::endl;
     
   } while (cont);
   
