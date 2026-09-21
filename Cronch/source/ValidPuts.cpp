d36e5e6c1ee43a04e450d08aaf3a957d8fcb251c
Cavan Theiss <cavanrt@gmail.com>
2026-09-21 08:55:38 -0500

    Create ValidPuts.cpp

---
+++ Cronch/source/ValidPuts.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************
+Title: ValidPuts
+Author: Cavan Ray Theiss
+Date: 09/21/2026
+Description/Purpose:
+
+General use safety net functions that can be used to validate any
+type of data input whether int, string, or whatever
+
+I'll be implementing multiple options for each data type, so you
+can just validate input or you can pair it with a message to the 
+user
+
+not all of these are going to be useful to Cronch, but I want this 
+in my toolbox for future programs I write
+
+*******************************************************************/
