a96ec716eea657bd3e11de34e8c4fadf87b0426b
Cavan Theiss <cavanrt@gmail.com>
2026-09-21 08:06:16 -0500

    Update 13 UsePuts.cpp
    13

--- Cronch/source/UsePuts.cpp
+++ Cronch/source/UsePuts.cpp
@@ -8,12 +8,14 @@ void optionsMenu(){
   do {
     clearScreen();
     std::cout << std::endl;
-std::cout << "|******   CCCC    R        OOOO     N    N    CCCC   H    H  ******|" << std::endl;
-std::cout << "|******  C    C   RRRR    O    O    NN   N   C    C  H    H  ******|" << std::endl;
-std::cout << "|****** C         R   R  O      O   N N  N  C        HHHHHH  ******|" << std::endl;
-std::cout << "|****** C         R      O      O   N  N N  C        H    H  ******|" << std::endl;
-std::cout << "|******  C    C   R       O    O    N   NN   C    C  H    H  ******|" << std::endl;
-std::cout << "|******   CCCC    R        OOOO     N    N    CCCC   H    H  ******|" << std::endl;
+    std::cout << " __________________________________________________________________ " << std::endl;
+    std::cout << "|******   CCCC    R        OOOO     N    N    CCCC   H    H  ******|" << std::endl;
+    std::cout << "|******  C    C   RRRR    O    O    NN   N   C    C  H    H  ******|" << std::endl;
+    std::cout << "|****** C         R   R  O      O   N N  N  C        HHHHHH  ******|" << std::endl;
+    std::cout << "|****** C         R      O      O   N  N N  C        H    H  ******|" << std::endl;
+    std::cout << "|******  C    C   R       O    O    N   NN   C    C  H    H  ******|" << std::endl;
+    std::cout << "|******   CCCC    R        OOOO     N    N    CCCC   H    H  ******|" << std::endl;
+    std::cout << "|__________________________________________________________________|" << std::endl;
     
   } while (cont);
   
