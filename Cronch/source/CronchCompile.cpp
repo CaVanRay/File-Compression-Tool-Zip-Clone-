397d13f4a11a5eea61cd27f280b05f788cba2b40
Cavan Theiss <cavanrt@gmail.com>
2026-09-22 16:33:23 -0500

    Update CronchCompile.cpp

--- Cronch/source/CronchCompile.cpp
+++ Cronch/source/CronchCompile.cpp
@@ -1,1 +1,11 @@
+/*******************************************************************
+Title: CronchCompile
+Author: Cavan Ray Theiss
+Date: 09/22/2026
+Description/Purpose:
+
+Main file for compiling sequence/functions
+
+*******************************************************************/
+
 #include "cronchHeaders.h"
