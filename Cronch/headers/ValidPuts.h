9b89de2224032a20a45beabf77740eead9cc6931
Cavan Theiss <cavanrt@gmail.com>
2026-09-22 16:56:58 -0500

    Update ValidPuts.h

--- Cronch/headers/ValidPuts.h
+++ Cronch/headers/ValidPuts.h
@@ -1,2 +1,19 @@
 #pragma once
 
+// Functions to validate inputs & index
+
+std::string getStringP(const std::string& prompt);
+
+std::string getString();
+
+std::string getStringS();
+
+int getInt(const std::string& prompt);
+
+double getDouble(const std::string& prompt);
+
+bool getYN(const std::string& prompt);
+
+bool isValidVin(car*& head, const std::string& vin);
+
+std::string getVIN(const std::string& prompt, car*& head);
