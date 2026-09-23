21497e95df23913c56f274782f7772b19ccbf3df
Cavan Theiss <cavanrt@gmail.com>
2026-09-23 16:37:07 -0500

    Update 22 CronchCompile.h
    22

--- Cronch/headers/CronchCompile.h
+++ Cronch/headers/CronchCompile.h
@@ -2,4 +2,4 @@
 
 // any functions required to compile declare here
 
-std::vector<uint8_t> read_binary_file(const std::string& filename)
+std::vector<uint8_t> read_binary_file(const std::string& filename);
