8377aec31ffe5e0b282048dcced81ff8d7db6482
Cavan Theiss <cavanrt@gmail.com>
2026-09-23 16:25:13 -0500

    Update 5 CronchCompile.cpp
    5

--- Cronch/source/CronchCompile.cpp
+++ Cronch/source/CronchCompile.cpp
@@ -16,5 +16,9 @@ std::vector<uint8_t> read_binary_file(const std::string& filename){
 
   // Open file in binary mode
   std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+  if (!file.is_open()){
+    
+  }
   
 }
