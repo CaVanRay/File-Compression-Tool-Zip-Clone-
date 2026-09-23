838cc12e647f357fbfcb38ca679db38b90cccd06
Cavan Theiss <cavanrt@gmail.com>
2026-09-23 16:29:43 -0500

    Update 13 CronchCompile.cpp
    13

--- Cronch/source/CronchCompile.cpp
+++ Cronch/source/CronchCompile.cpp
@@ -25,6 +25,6 @@ std::vector<uint8_t> read_binary_file(const std::string& filename){
   std::streamsize size + file.tellg();
   file.seekg(0, std::ios::beg);
 
-  // Resize vector tofit file content
+  // Resize vector to fit file content
   std::vector<uint8_t> buffer(static_cast<size_t>(size));
 }
