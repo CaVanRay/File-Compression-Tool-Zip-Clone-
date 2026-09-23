11efdf3429008e23a343e089c254e26f25aa37f1
Cavan Theiss <cavanrt@gmail.com>
2026-09-23 16:38:29 -0500

    Update 23 CronchCompile.h
    23

--- Cronch/headers/CronchCompile.h
+++ Cronch/headers/CronchCompile.h
@@ -3,3 +3,5 @@
 // any functions required to compile declare here
 
 std::vector<uint8_t> read_binary_file(const std::string& filename);
+
+void count_frequencies(const std::vector<uint8_t>& data, uint32_t freq[256]);
